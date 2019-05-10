bec5f89c673e406ab4bba006dd2831c00908da35
Maxum Smith <Maxum.Smith@healthcarousel.com>
2019-05-10 12:10:50 -0400

    ci
    ci

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -44,11 +44,11 @@ namespace GradeBook.UserInterfaces
             BaseGradeBook gradeBook;
             if (type == "standard")
             {
-                gradeBook = new StandardGradeBook(name);
+                gradeBook = new StandardGradeBook(name, Convert.ToBoolean(parts[3]));
             }
             else if (type == "ranked")
             {
-                gradeBook = new RankedGradeBook(name);
+                gradeBook = new RankedGradeBook(name, Convert.ToBoolean(parts[3]));
             }
             else
             {
